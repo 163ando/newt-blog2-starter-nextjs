65bcad85e2d3e36700fa392fa6f0116a6fe5016f
Hiromi Ando <hrm289leo@gmail.com>
2023-05-22 11:01:47 +0700

    Update Side.tsx

--- components/Side.tsx
+++ components/Side.tsx
@@ -131,7 +131,6 @@ export function Side({
                     <img
                       src={banner.bannerImage.src}
                       alt=''
-                      target={banner.bannerLinkTarget}
                     />
                   )}
                 </a>
